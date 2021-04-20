83be3f4ac7dda29004e36b641d30da3ab0f3a97c
THIRUMALA SRIKANTH HAYAGREEVA CHARYA <69465021+TSKHayagreevaC@users.noreply.github.com>
2021-04-20 17:03:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
-
+import os
 
 # ---------------- PASSWORD GENERETOR -----------------
 def generate_password():
